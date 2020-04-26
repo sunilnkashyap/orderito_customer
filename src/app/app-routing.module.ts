c16adda812f3e95e01eec4eb80e43cb3cb202622
skdroid <sunilnkashyap@gmail.com>
2020-04-26 15:34:20 +0530

    home page integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    { path: 'splash', loadChildren: './splash/splash.module#SplashPageModule' },
+    { path: '', loadChildren: './splash/splash.module#SplashPageModule' },
     { path: 'h', loadChildren: './tabs/tabs.module#TabsPageModule' },
     {
         path: 'givereview',
         loadChildren: './givereview/givereview.module#GivereviewPageModule'
     },
-  { path: '', loadChildren: './dashboard/dashboard.module#DashboardPageModule' }
+  { path: 'skdash', loadChildren: './dashboard/dashboard.module#DashboardPageModule' }
 
-  
+
 ];
 
 @NgModule({
